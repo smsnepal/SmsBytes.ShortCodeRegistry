7a1237c1a03ac4b0aee96d9510ba591e428bdde6
Nishchal <gautam.nishchal@gmail.com>
2021-06-20 16:39:16 +0700

    fix(gqlClient): use endpoint from configuration

--- SmsBytes.ShortCodeRegistry.Api/Internal/Configs/Services.cs
+++ SmsBytes.ShortCodeRegistry.Api/Internal/Configs/Services.cs
@@ -3,9 +3,14 @@ namespace SmsBytes.ShortCodeRegistry.Api.Internal.Configs
     public class Services
     {
         public KeyStoreConfig KeyStore { set; get; }
+        public AppRegistration AppRegistration { set; get; }
     }
 
     public class KeyStoreConfig
+    {
+        public string Url { set; get; }
+    }
+    public class AppRegistration
     {
         public string Url { set; get; }
     }
--- SmsBytes.ShortCodeRegistry.Api/Internal/StartupExtensions/DependencyInjection.cs
+++ SmsBytes.ShortCodeRegistry.Api/Internal/StartupExtensions/DependencyInjection.cs
@@ -1,9 +1,10 @@
 using Microsoft.Extensions.Configuration;
-using SmsBytes.ShortCodeRegistry.Storage;
 using Microsoft.Extensions.DependencyInjection;
+using SmsBytes.ShortCodeRegistry.Api.Internal.Configs;
 using SmsBytes.ShortCodeRegistry.Business;
 using SmsBytes.ShortCodeRegistry.Business.ApplicationInfo;
 using SmsBytes.ShortCodeRegistry.Common.Uuid;
+using SmsBytes.ShortCodeRegistry.Storage;
 
 namespace SmsBytes.ShortCodeRegistry.Api.Internal.StartupExtensions
 {
@@ -17,6 +18,12 @@ namespace SmsBytes.ShortCodeRegistry.Api.Internal.StartupExtensions
             services.AddTransient<IShortCodeRepository, ShortCodeRepository>();
             services.AddTransient<IShortCodeRegistryService, ShortCodeRegistryService>();
             services.AddTransient<IApplicationInfoService, ApplicationInfoService>();
+            var appRegistrationUrl = configuration
+                .GetSection("Services")
+                .Get<Services>()
+                .AppRegistration
+                .Url;
+            services.AddSingleton<IApplicationGraphqlClient>(new ApplicationGraphqlClient(appRegistrationUrl));
         }
     }
 }
